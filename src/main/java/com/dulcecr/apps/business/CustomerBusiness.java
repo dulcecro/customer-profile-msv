06e48943fbfda588fccc8bc2e101321b90a0a527
dcanchari <dtcancharir@gmail.com>
2026-06-26 00:31:30 -0500

    feat: add contract first

---
+++ src/main/java/com/dulcecr/apps/business/CustomerBusiness.java
@@ -0,0 +1,15 @@
+package com.dulcecr.apps.business;
+
+/**
+ * <b>Class</b>: CustomerBusiness<br/>
+ * <<ADD DESCRIPTION>> <br/>
+ *
+ * <b>Developed by</b>: Dulce Canchari <br/>
+ * <b>Date</b>: 26/06/2026<br/>
+ *
+ * @version 1.0
+ */
+
+public class CustomerBusiness {
+
+}
---
+++ src/main/java/com/dulcecr/apps/business/impl/CustomerBusinessImpl.java
@@ -0,0 +1,14 @@
+package com.dulcecr.apps.business.impl;
+
+/**
+ * <b>Class</b>: CustomerBusinessImpl<br/>
+ * <<ADD DESCRIPTION>> <br/>
+ *
+ * <b>Developed by</b>: Dulce Canchari <br/>
+ * <b>Date</b>: 26/06/2026<br/>
+ *
+ * @version 1.0
+ */
+ 
+public interface CustomerBusinessImpl {
+}
